d4365c519d4fcd1282067b129f1918831f8a1a36
Oskar Mikolajczyk <omikolaj1@gmail.com>
2022-01-09 16:30:30 -0500

    refactor: remove unused components

--- src/app/views/users/email-confirmed/email-confirmed.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-	selector: 'odm-email-confirmed',
-	templateUrl: './email-confirmed.component.html',
-	styleUrls: ['./email-confirmed.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class EmailConfirmedComponent {}
--- src/app/views/users/successful-registration/successful-registration.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-	selector: 'odm-successful-registration',
-	templateUrl: './successful-registration.component.html',
-	styleUrls: ['./successful-registration.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class SuccessfulRegistrationComponent implements OnInit {}
